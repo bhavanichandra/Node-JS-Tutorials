a4f4e57e074fba481335942e5c0c0f6527fb6432
Bhavani Chandra <bhavanichandra9@live.co.uk>
2020-12-28 02:01:50 +0530

    added order logic with mongodb

--- controllers/shop.js
+++ controllers/shop.js
@@ -77,38 +77,17 @@ exports.postCartDelete = (req, res, next) => {
 };
 
 exports.postOrder = (req, res, next) => {
-	let fetchedCart;
 	req.user
-		.getCart()
-		.then((cart) => {
-			fetchedCart = cart;
-			return cart.getProducts();
-		})
-		.then((products) => {
-			return req.user
-				.createOrder()
-				.then((order) => {
-					return order.addProducts(
-						products.map((product) => {
-							product.orderItem = { quantity: product.cartItem.quantity };
-							return product;
-						})
-					);
-				})
-				.then((result) => {
-					return fetchedCart.setProducts(null);
-				})
-				.then((result) => {
-					res.redirect('/orders');
-				})
-				.catch((err) => console.log(err));
+		.addOrder()
+		.then((result) => {
+			res.redirect('/orders');
 		})
 		.catch((err) => console.log(err));
 };
 
 exports.getOrders = (req, res, next) => {
 	req.user
-		.getOrders({ include: ['products'] })
+		.getOrders()
 		.then((orders) => {
 			res.render('shop/orders', {
 				path: '/orders',
--- models/user.js
+++ models/user.js
@@ -74,7 +74,33 @@ class User {
 	}
 
 	addOrder() {
-		
+		const db = getDb();
+		return this.getCart()
+			.then((products) => {
+				const order = {
+					items: products,
+					user: {
+						_id: new mongodb.ObjectID(this._id),
+						name: this.name,
+						email: this.email
+					}
+				};
+				return db.collection('orders').insertOne(order);
+			})
+			.then((result) => {
+				this.cart = { items: [] };
+				return db
+					.collection('users')
+					.updateOne(
+						{ _id: new mongodb.ObjectID(this._id) },
+						{ $set: { cart: { items: [] } } }
+					);
+			});
+	}
+
+	getOrders() {
+		const db = getDb();
+		return db.collection('orders').find().toArray();
 	}
 
 	static findUserById(userId) {
