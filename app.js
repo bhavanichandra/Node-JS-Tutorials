bf1d754a19c230e4ef5651b8d0e842e6b6f03c4f
Bhavani Chandra <bhavanichandra9@live.co.uk>
2020-12-28 12:23:27 +0530

    Added Login Page to the app

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const authRoutes = require('./routes/auth');
 
 const env = require('dotenv').config();
 
@@ -35,7 +36,7 @@ if (env.error) {
 
 	app.use('/admin', adminRoutes);
 	app.use(shopRoutes);
-
+	app.use(authRoutes);
 	app.use(errorController.get404);
 
 	mongoose
