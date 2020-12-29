f641bb978eb9d20126fcc890f0c64e0e9b110fa4
Bhavani Chandra <bhavanichandra9@live.co.uk>
2020-12-29 12:41:56 +0530

    Added Client Side JS to Remove Product without reloading page

--- controllers/admin.js
+++ controllers/admin.js
@@ -176,8 +176,8 @@ exports.getProducts = (req, res, next) => {
 		});
 };
 
-exports.postDeleteProduct = (req, res, next) => {
-	const prodId = req.body.productId;
+exports.deleteProduct = (req, res, next) => {
+	const prodId = req.params.productId;
 	Product.findById(prodId)
 		.then((prod) => {
 			if (!prod) {
@@ -188,12 +188,10 @@ exports.postDeleteProduct = (req, res, next) => {
 		})
 		.then(() => {
 			console.log('DESTROYED PRODUCT');
-			res.redirect('/admin/products');
+			res.status(200).json({ message: 'Deleted Product!' });
 		})
 		.catch((err) => {
 			console.log(err);
-			const error = new Error(err);
-			error.httpStatusCode = 500;
-			return next(error);
+			res.status(500).json({ message: 'Deleting Product Failed' });
 		});
 };
