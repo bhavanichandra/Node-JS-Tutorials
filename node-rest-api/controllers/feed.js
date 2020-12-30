9e42022d2647195fcc6e498d9cdf9da682e9d74a
Bhavani Chandra <bhavanichandra9@live.co.uk>
2020-12-30 20:57:36 +0530

    Add Delete Route

--- node-rest-api/controllers/feed.js
+++ node-rest-api/controllers/feed.js
@@ -137,6 +137,32 @@ exports.editPost = (req, res, next) => {
 		});
 };
 
+exports.deletePost = (req, res, next) => {
+	const postId = req.params.postId;
+
+	Post.findById(postId)
+		.then((post) => {
+			// Check loggedin user
+			if (!post) {
+				const error = new Error('Could not found error');
+				error.statusCode = 404;
+				throw error;
+			}
+			clearImage(post.imageUrl);
+			return Post.findByIdAndRemove(postId);
+		})
+		.then((result) => {
+			console.log(result);
+			res.status(200).json({ message: 'Post Deleted' });
+		})
+		.catch((err) => {
+			if (!err.statusCode) {
+				err.statusCode = 500;
+			}
+			next(err);
+		});
+};
+
 const clearImage = (filePath) => {
 	const fullPath = path.join(__dirname, '..', filePath);
 	fs.unlink(fullPath, (err) => console.log(err));
--- node-rest-api/routes/feed.js
+++ node-rest-api/routes/feed.js
@@ -27,4 +27,6 @@ router.put(
 	feedController.editPost
 );
 
+router.delete('/post/:postId', feedController.deletePost);
+
 module.exports = router;
